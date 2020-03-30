769e7b1a098f5c8b5495754c667f7f1e87ad3f80
IgorBrykin <igorbrikin@gmail.com>
2020-03-30 23:32:49 +0300

    Arrays

---
+++ src/jr/task0620/Task0701.java
@@ -0,0 +1,35 @@
+package jr.task0620;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task0701 {
+    public static void main(String[] args) throws Exception {
+        int[] array = initializeArray();
+        int max = max(array);
+        System.out.println(max);
+    }
+
+    public static int[] initializeArray() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] list = new int[20];
+        for (int i = 0; i < list.length; i++) {
+//            String s = reader.readLine();
+//            list[i] = Integer.parseInt(s);
+            list[i] = Integer.parseInt(reader.readLine());
+        }
+        return list;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+        }
+        return max;
+    }
+}
