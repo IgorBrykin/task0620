bc1aeeee8f3e64dabfba8fc7ca4f1940d0780da5
IgorBrykin <igorbrikin@gmail.com>
2020-03-31 02:51:53 +0300

    Arrays Запись длины массива

---
+++ src/jr/tasks/Task0704.java
@@ -0,0 +1,17 @@
+package jr.tasks;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Task0704 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for (int i = arr.length; i > 0; i--) {
+            System.out.println(arr[i-1]);   // переворот масссива
+        }
+    }
+}
