a46567c54c0c7212cfec6530f69c240cd8ace816
IgorBrykin <igorbrikin@gmail.com>
2020-03-31 21:43:26 +0300

    addArrays

--- src/jr/tasks/Test0705.java
+++ src/jr/tasks/Test0705.java
@@ -1,4 +1,26 @@
 package jr.tasks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Test0705 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[20];
+        int[] halfOne = new int[10];
+        int[] halfTwo = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+        for (int i = 0; i < halfOne.length; i++) {
+            halfOne[i] = a[i];
+        }
+        for (int i = 0; i < halfTwo.length; i++) {
+            halfTwo[i] = a[i + 10];// Запоолняем дальше массивы после 10++
+        }
+        for (int i : halfTwo) {
+            System.out.println(i); // посредством iter выводим halfTwo
+        }
+    }
 }
