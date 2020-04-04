3d122276749c245c42b2b6d06754bc76c5109847
IgorBrykin <igorbrikin@gmail.com>
2020-04-04 04:11:06 +0300

    ArrayList

---
+++ src/jr/tasks/Task0717.java
@@ -0,0 +1,50 @@
+package jr.tasks;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+/*
+Удваиваем слова
+1. Введи с клавиатуры 10 слов в список строк.
+
+2. Метод doubleValues должен удваивать слова по принципу:
+"альфа", "бета", "гамма" -> "альфа", "альфа", "бета", "бета", "гамма", "гамма"
+
+3. Выведи результат на экран, каждое значение с новой строки.
+
+
+Требования:
+1. Объяви переменную типа список строк и сразу проинициализируй ee.
+2. Считай 10 строк с клавиатуры и добавь их в список.
+3. Метод doubleValues должен удваивать элементы списка по принципу "альфа", "бета", "гамма" -> "альфа", "альфа", "бета", "бета", "гамма", "гамма".
+4. Выведи получившийся список на экран, каждый элемент с новой строки.
+*/
+
+
+public class Task0717 {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+//            String s = br.readLine();
+//            list.add(s);
+            list.add(br.readLine());    // скращенный вариант
+        }
+
+        ArrayList<String> result = doubleValues(list);
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i += 2) { // i+=2 перепрыгивает
+            list.add(i, list.get(i));
+            // i++; // итерация ++ второй вариант
+        }
+        return list;
+    }
+}
