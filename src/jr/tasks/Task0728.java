35014cef1a2f781f7e76d47f6b1b9089c8115512
IgorBrykin <igorbrikin@gmail.com>
2020-04-06 00:09:00 +0300

    ArrayList

--- src/jr/tasks/Task0728.java
+++ src/jr/tasks/Task0728.java
@@ -1,71 +1,46 @@
 package jr.tasks;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 /*
-Меняем функциональность
-
-Задача: Программа вводит строки, пока пользователь не
-введёт пустую строку (нажав enter). Потом она конвертирует
-строки в верхний регистр (Мама превращается в МАМА) и выводит их на экран.
-
-Новая задача: Программа вводит строки, пока пользователь не введёт
- пустую строку (нажав enter).
-Потом программа строит новый список. Если в строке чётное число букв,
- строка удваивается, если нечётное - утраивается.
-Программа выводит содержимое нового списка на экран.
-
-Пример ввода:
-Кот
-Коты
-Я
-
-Пример вывода:
-Кот Кот Кот
-Коты Коты
-Я Я Я
+В убывающем порядке
+Задача: Написать программу, которая вводит
+с клавиатуры 20 чисел и выводит их в убывающем порядке.
 
 
 Требования:
-1. Программа должна считывать данные с клавиатуры.
-2. Программа должна вывести столько же строк,
- сколько было введено.
-3. Если в строке было четное число букв,
-то необходимо удвоить строку (как в примере).
-4. Если в строке было нечетное число букв,
-то необходимо утроить строку (как в примере).
-5. Программа должна прекращать считывать
-данные с клавиатуры как только пользователь введет пустую строку и нажмет Enter.
+1. Программа должна считывать 20 целых чисел с клавиатуры.
+2. Программа должна выводить 20 чисел.
+3. В классе Solution должен быть метод public static void sort(int[] array).
+4. Метод main должен вызывать метод sort.
+5. Метод sort должен сортировать переданный массив по убыванию.
 */
 
-public class Task0728 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-        ArrayList<String> strings = new ArrayList<String>();
-        while (true) {
-            String string = reader.readLine();
-            if (string == null || string.isEmpty()) break;
-            strings.add(string);
+public class Task0728 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
         }
 
-//        ArrayList<String> resultStrings = new ArrayList<String>();
-//        for (int i = 0; i < strings.size(); i++) {
-//            String string = strings.get(i);
-//            resultStrings.add(string.toUpperCase());
-//        }
-//
-//        for (int i = 0; i < resultStrings.size(); i++) {
-//            System.out.println(resultStrings.get(i));
-//        }
-        for (String string : strings) {         //пробежавшись, выводим соответственно
-            if (string.length() % 2 == 0) {
-                System.out.println(string + " " + string);
-            }
-            if (string.length() % 2 != 0) {
-                System.out.println(string + " " + string + " " + string);
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) { //это способоб сортировки
+                if (array[j] > array[i]) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
             }
         }
     }
