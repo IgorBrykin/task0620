f913c0f5feef271923aa06542d5c2c76a5529ed5
IgorBrykin <igorbrikin@gmail.com>
2020-04-05 17:17:12 +0300

    ArrayList

---
+++ src/jr/tasks/Task0722.java
@@ -0,0 +1,39 @@
+package jr.tasks;
+
+/*
+Это конец
+Создать список строк.
+Ввести строки с клавиатуры и добавить их в список.
+Вводить с клавиатуры строки, пока пользователь не введет строку "end". Саму строку "end" не учитывать.
+Вывести строки на экран, каждую с новой строки.
+
+
+Требования:
+1. Объяви переменную типа список строк и сразу проинициализируй ee.
+2. Считывай строки с клавиатуры и добавляй их в список, пока пользователь не введет строку "end".
+3. Саму строку "end" не нужно добавлять в список.
+4. Выведи список на экран, каждое значение с новой строки.
+5. Используй цикл for.
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Task0722 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> arrayList = new ArrayList<>();
+        String a = "end";
+        while (true){
+            String s = reader.readLine();
+            if (s.equals(a))    // сравниваем строки
+                break;
+            arrayList.add(s);
+        }
+        for (String s : arrayList) {
+            System.out.println(s);
+        }
+    }
+}
