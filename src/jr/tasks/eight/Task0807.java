95a30f049d048c2c0321fe88047a093e923075bc
IgorBrykin <igorbrikin@gmail.com>
2020-04-08 14:03:43 +0300

    Iterator

---
+++ src/jr/tasks/eight/Task0807.java
@@ -0,0 +1,36 @@
+package jr.tasks.eight;
+
+/*
+LinkedList и ArrayList
+Нужно создать два списка - LinkedList и ArrayList.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Программа не должна считывать значения с клавиатуры.
+3. Программа должна содержать только три метода.
+4. Метод createArrayList()
+должен создавать и возвращать список ArrayList.
+5. Метод createLinkedList()
+должен создавать и возвращать список LinkedList.
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Task0807 {
+    public static Object createArrayList() {
+        ArrayList <Object> arrayList = new ArrayList<>();
+        return arrayList;
+    }
+
+    public static Object createLinkedList() {
+        LinkedList<Object> linkedList = new LinkedList<>();
+        return linkedList;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
