6ecc485330d3ee4af40d21e790e8edfb7e6fe103
IgorBrykin <igorbrikin@gmail.com>
2020-04-19 13:34:20 +0300

    Static

--- src/jr/tasks/task09/task09/task0903/Solution.java
+++ src/jr/tasks/task09/task09/task0903/Solution.java
@@ -1,4 +1,4 @@
-package jr.tasks.task09.task0903;
+package jr.tasks.task09.task09.task0903;
 
 /* 
 Кто меня вызывал?
@@ -23,29 +23,29 @@ public class Solution {
     public static int method1() {
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
         method2();
-        return trace[2].getLineNumber();
+        return  trace[2].getLineNumber();
     }
 
     public static int method2() {
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
         method3();
-        return trace[2].getLineNumber();
+        return  trace[2].getLineNumber();
     }
 
     public static int method3() {
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
         method4();
-        return trace[2].getLineNumber();
+        return  trace[2].getLineNumber();
     }
 
     public static int method4() {
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
         method5();
-        return trace[2].getLineNumber();
+        return  trace[2].getLineNumber();
     }
 
     public static int method5() {
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
-        return trace[2].getLineNumber();
+        return  trace[2].getLineNumber();
     }
 }
